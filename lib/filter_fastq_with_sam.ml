pveber/te-insertion-detector

commit b859fbea1f5fb6c1d6431272eb7bc532cb45dc87

fixed recognition of reads of the same pair

lib/filter_fastq_with_sam.ml
@@ -2,8 +2,19 @@ open Core
 open Biocaml_ez
 open CFStream
 
+let left_side x =
+  match String.lsplit2 ~on:' ' x with
+  | Some (x, _) -> x
+  | None -> x
+
 let chop s =
   String.sub s ~pos:0 ~len:(String.length s - 2)
+
+let normalize_id x =
+  let x = left_side x in
+  if String.is_suffix ~suffix:"/1" x && String.is_suffix ~suffix:"/2" x then
+    chop x
+  else x
 
 let idset_of_sam sam_path =
   In_channel.with_file sam_path ~f:(fun ic ->
@@ -12,7 +23,7 @@ let idset_of_sam sam_path =
       Stream.iter items ~f:(fun al ->
           match al.Sam.qname, al.Sam.mapq with
           | Some qname, Some mapq when mapq > 30 ->
-            let qname = chop qname in
+            let qname = normalize_id qname in
             Hash_set.add set qname
           | _ -> ()
         ) ;
@@ -22,7 +33,7 @@ let idset_of_sam sam_path =
 let filter_fastq pred set ic oc =
   Fastq.read ic
   |> Stream.filter ~f:(fun it ->
-      let name = chop it.Biocaml_unix.Fastq.name in
+      let name = normalize_id it.Biocaml_unix.Fastq.name in
       pred (Hash_set.mem set) name
     )
   |> Fastq.write oc

lib/misc.ml
@@ -29,7 +29,7 @@ let gzdest =
   ]
 
 let filter_fastq_with_sam ?invert (sam : sam workflow) (fq : 'a fastq gz workflow) : 'a fastq gz workflow =
-  workflow ~descr:"filter_fastq_with_sam" [
+  workflow ~descr:"filter_fastq_with_sam" ~version:1 [
     cmd "bash" [
       file_dump (seq ~sep:" " [
           string "te-insertion-detector" ;

commit 4277fa96f5857e8c53292d5923d8d1b07e1421cd

fixed bug introduced in b859fbe

lib/filter_fastq_with_sam.ml
@@ -12,7 +12,7 @@ let chop s =
 
 let normalize_id x =
   let x = left_side x in
-  if String.is_suffix ~suffix:"/1" x && String.is_suffix ~suffix:"/2" x then
+  if String.is_suffix ~suffix:"/1" x || String.is_suffix ~suffix:"/2" x then
     chop x
   else x
 

lib/misc.ml
@@ -29,7 +29,7 @@ let gzdest =
   ]
 
 let filter_fastq_with_sam ?invert (sam : sam workflow) (fq : 'a fastq gz workflow) : 'a fastq gz workflow =
-  workflow ~descr:"filter_fastq_with_sam" ~version:1 [
+  workflow ~descr:"filter_fastq_with_sam" ~version:2 [
     cmd "bash" [
       file_dump (seq ~sep:" " [
           string "te-insertion-detector" ;