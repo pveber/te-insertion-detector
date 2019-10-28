2bdd6f968ecaa43ac1436ee268539fcfcf255024
Philippe Veber <philippe.veber@gmail.com>
2019-10-28 15:30:36 +0100

    Pipeline/species: added TE library inputs

--- lib/pipeline/species.ml
+++ lib/pipeline/species.ml
@@ -8,3 +8,11 @@ let annotation s : gff3 pworkflow =
   | `Dmel ->
     Bistro_unix.wget "ftp://ftp.flybase.net/genomes/Drosophila_melanogaster/dmel_r6.29_FB2019_04/gtf/dmel-all-r6.29.gtf.gz"
     |> Bistro_unix.gunzip
+
+let reduced_te_library = function
+  | `Dmel ->
+    "data/reduced_melanogaster_te_list.fa"
+
+let te_library = function
+  | `Dmel ->
+    "data/melanogaster_te_list.fa"
