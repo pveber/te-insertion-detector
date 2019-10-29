1ff5eacfa04d6d782c05703037b150bac30068f5
Philippe Veber <philippe.veber@gmail.com>
2019-10-29 15:17:24 +0100

    pipeline/Species: fixed mel genomes

--- lib/pipeline/species.ml
+++ lib/pipeline/species.ml
@@ -6,7 +6,7 @@ type t = [`Dmel | `Dsim]
 let annotation s : gff3 pworkflow =
   Bistro_unix.wget (
     match s with
-    | `Dmel -> "ftp://ftp.flybase.net/genomes/Drosophila_melanogaster/dmel_r6.29_FB2019_04/gtf/dmel-all-r6.29.gtf.gz"
+    | `Dmel -> "ftp://ftp.flybase.net/genomes/Drosophila_melanogaster/dmel_r6.16_FB2017_03/gtf/dmel-all-r6.16.gtf.gz"
     | `Dsim -> "ftp://ftp.flybase.net/genomes/Drosophila_simulans/dsim_r2.02_FB2017_04/gtf/dsim-all-r2.02.gtf.gz"
   )
   |> Bistro_unix.gunzip
@@ -14,15 +14,19 @@ let annotation s : gff3 pworkflow =
 let reduced_te_library = function
   | `Dmel ->
     "data/reduced_melanogaster_te_list.fa"
+  | `Dsim ->
+    assert false
 
 let te_library = function
   | `Dmel ->
     "data/melanogaster_te_list.fa"
+  | `Dsim ->
+    assert false
 
 let genome s : fasta pworkflow =
   Bistro_unix.wget (
     match s with
-    | `Dmel -> "ftp://ftp.flybase.net/genomes/Drosophila_melanogaster/dmel_r6.29_FB2019_04/fasta/dmel-all-chromosome-r6.29.fasta.gz"
+    | `Dmel -> "ftp://ftp.flybase.net/genomes/Drosophila_melanogaster/dmel_r6.16_FB2017_03/fasta/dmel-all-chromosome-r6.16.fasta.gz"
     | `Dsim -> "ftp://ftp.flybase.net/genomes/Drosophila_simulans/dsim_r2.02_FB2017_04/fasta/dsim-all-chromosome-r2.02.fasta.gz"
   )
   |> Bistro_unix.gunzip
