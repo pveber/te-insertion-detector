b9731a7738b054c6efdb4f5090a6fad42c6d12b6
Philippe Veber <philippe.veber@gmail.com>
2019-10-29 07:22:15 +0100

    app: added WIP mode

--- app/te_insertion_detector_app.ml
+++ app/te_insertion_detector_app.ml
@@ -1,6 +1,24 @@
 open Core
 open Te_insertion_detector
 
+include Bistro_utils.Toplevel_eval.Make(struct
+  let np = 7
+  let mem = 10
+end)()
+
+let wip_main () =
+  let open Te_insertion_detector_pipeline in
+  try
+    let te_library =
+      Te_insertion_detector.Misc.load_transposable_elements (Species.te_library `Dmel)
+    in
+    Dna_sample.annotated_insertions `Dmel te_library
+    |> less
+  with Failure msg -> print_endline msg
+
+let wip_command = Command.basic ~summary:"WIP" (Command.Param.return wip_main)
+
+
 let () = Command.(
     group ~summary:"Transposable Element Insertion Detector" [
       "detection", Pipeline_alt.detection_command ;
@@ -10,6 +28,7 @@ let () = Command.(
       "insertions-in-fasta", Insertions_in_fasta.command ;
       "match-insertions", Match_insertions.command ;
       "droso-te-fasta-format", Droso_te_fasta_format.command ;
+      "wip", wip_command ;
     ]
     |> run
   )
